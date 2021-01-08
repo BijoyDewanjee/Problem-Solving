2f1c6bfb740a62bef2a18e98308afcb32e51194b
BijoyDewanjee <66883889+BijoyDewanjee@users.noreply.github.com>
2021-01-08 19:48:41 +0600

    Create A. Fence.cpp

---
+++ A. Fence.cpp
@@ -0,0 +1,12 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long int t,a,b,c,r;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        r=(a+b+c)-1;
+        cout<<r<<endl;
+    }
+}
